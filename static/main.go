1eb0784c212c6f7e0aecf58e415eb362fa194c8e
Ubuntu <ubuntu@ip-172-31-11-7.ap-south-1.compute.internal>
2026-01-11 12:23:51 +0000

    Fix app routes and add root and health endpoints

---
+++ static/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Go Web App is running 🚀")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "OK")
+	})
+
+	log.Println("Starting server on port 8080")
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+}
+
